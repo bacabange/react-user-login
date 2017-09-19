004b0ad470dfb4edf86e23a8884c945a9584207e
Stiven Castillo <stiven.castillo@creardigital.com>
2017-09-19 15:18:14 -0500

    Flux, home and login views

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import {
+    Route,
+    BrowserRouter as Router
+} from 'react-router-dom';
+
+import Home from './views/Home';
+import Login from './views/Login';
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <div>
+                    <Route exact path="/" component={Login} />
+                    <Route path="/home" component={Home} />
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
