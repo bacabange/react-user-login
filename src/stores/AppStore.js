a1aa61891d0c4ab34e8a893ebcc013beb1dc30ff
Stiven Castillo <stiven.castillo@creardigital.com>
2017-09-19 15:22:17 -0500

    Flux, home and login views

---
+++ src/stores/AppStore.js
@@ -0,0 +1,12 @@
+import Reflux from 'reflux';
+import Actions from '../actions/Actions';
+
+class AppStore extends Reflux.store {
+    constructor() {
+        super();
+
+        this.listenables = Actions;
+    }
+}
+
+export default AppStore;
