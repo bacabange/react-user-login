660feecb8ef0b64dd30d02421e812cdebce48dfb
Stiven Castillo <stiven.castillo@creardigital.com>
2017-09-19 16:33:51 -0500

    Login box

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,6 @@ class Navbar extends Component {
         return (
             <div>
                 <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                    <a className="navbar-brand" href="#"></a>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
--- src/views/Login.js
+++ src/views/Login.js
@@ -1,13 +1,60 @@
 import React, { Component } from 'react';
+import { Link, Redirect } from 'react-router-dom';
 
 class Login extends Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            username: '',
+            password: '',
+            isLogged: false
+        }
+
+        this.onSubmit = this.onSubmit.bind(this);
     }
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        if (this.state.username == 'admin' && this.state.password == 'admin') {
+            this.setState({ username: '', password: '', isLogged: true });
+        } else {
+            alert('Inválido!');
+            this.setState({ username: '', password: '', isLogged: false });
+        }
+    }
+
     render() {
         return (
             <div className="Login">
-                <h1>Login puto</h1>
+                { this.state.isLogged ?
+                    <Redirect to="/home" />
+                    :
+                    <div className="d-flex justify-content-center">
+                        <form onSubmit={ this.onSubmit }>
+                            <div className="form-group">
+                                <label>Username</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    value={ this.state.username}
+                                    onChange={ (e) => { this.setState({username: e.target.value})} }
+                                    />
+                            </div>
+                            <div className="form-group">
+                                <label>Password</label>
+                                <input
+                                    type="password"
+                                    className="form-control"
+                                    value={ this.state.password}
+                                    onChange={ (e) => { this.setState({password: e.target.value})} }
+                                    />
+                            </div>
+                            <button type="submit" className="btn btn-primary">Submit</button>
+                        </form>
+                    </div>
+                }
             </div>
         );
     }
